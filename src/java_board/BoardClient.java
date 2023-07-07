42a6851bb4a63e5830f08a07fd4ae297ce9a06a3
Choi-Yuchan <cor330@gmail.com>
2023-07-07 17:28:21 +0900

    java_console board class created

--- src/java_board/BoardClient.java
+++ src/java_board/BoardClient.java
@@ -1,9 +1,24 @@
 package java_board;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class BoardClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String serverIP = "127.0.0.1";
+
+		Socket socket = null;
+
+		try {
+
+			socket = new Socket(serverIP, 8888);
+
+			System.out.println("Connected at server.");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
--- src/java_board/BoardServer.java
+++ src/java_board/BoardServer.java
@@ -1,10 +1,28 @@
 package java_board;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class BoardServer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ServerSocket serverSocket = null;
+
+		Socket socket = null;
+
+		try {
+			// Port number
+			serverSocket = new ServerSocket(8888);
+			System.out.println("Server is ready.");
+
+			socket = serverSocket.accept();
+			System.out.println("Connected." + socket.getInetAddress().getHostName());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
